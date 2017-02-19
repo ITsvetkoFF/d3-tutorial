32e57f79793619f4774125487d8cb9710acfa87c
itsvetkoff <tsvetkoff@i.ua>
2017-02-19 15:48:18 +0200

    lesson0 is really useful

--- lesson0/script.ts
+++ lesson0/script.ts
@@ -1,87 +1,36 @@
 /// <reference types='../node_modules/@types/d3' />
 
-let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+let body = d3.select("body");
+let model = body.append("input").attr("type", "text");
+let table = body.append("table");
+let tableRow = table.append("tr");
+d3.range(9).forEach(() => table.append("tr"));
 
-let svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    g = svg.append("g");
+let dsv =  d3.dsvFormat(",");
 
-let alphabetInput = d3.select("#alphabet");
-let posRange = d3.select("#nPos");
-
-// when the input range changes update the circle
-posRange.on("input", function() {
-    updatePosition(+(<HTMLInputElement>this).value);
+model.on("input", function() {
+    let text = (<HTMLInputElement>this).value;
+    // let numericArray = text.split("");
+    let numericArray = dsv.parseRows(text)[0];
+    drawTable(numericArray);
 });
 
-// Initial starting radius of the circle
-updatePosition(50);
-
-// update the elements
-function updatePosition(nPos) {
-
-    // adjust the text on the range slider
-    d3.select("#nPos-value").text(nPos);
-    posRange.property("value", nPos);
-
-    // update the circle radius
-    g.attr("transform", `translate(32,${nPos})`);
-}
-
-function update(data) {
-
-    let t = (<any>d3).transition().duration(750); //??
-
-    // JOIN new data with old elements.
-    let text = g.selectAll("text")
+function drawTable(data) {
+    let columns = tableRow
+        .selectAll("td")
         .data(data, (d: string) => d );
 
-    // EXIT old elements not present in new data.
-    text.exit()
-        .attr("class", "exit")
-        .transition(t)
-        .attr("y", 60)
-        .style("fill-opacity", 1e-6)
-        .remove();
+    columns.exit().remove();
 
-    // UPDATE old elements present in new data.
-    text.attr("class", "update")
-        .attr("y", 0)
-        .style("fill-opacity", 1)
-        .transition(t)
-        .attr("x", function(d, i) { return i * 32; });
+    let cell = columns.enter()
+        .append("td")
+        .attr("valign", "top");
+    cell.append("input");
+    cell.append("span");
 
-    // ENTER new elements present in new data.
-    text.enter().append("text")
-        .attr("class", "enter")
-        .attr("dy", ".35em")
-        .attr("y", -60)
-        .attr("x", function(d, i) { return i * 32; })
-        .style("fill-opacity", 1e-6)
-        .text(function(d) { return d; })
-        .transition(t)
-        .attr("y", 0)
-        .style("fill-opacity", 1);
+    let mergedCell = cell.merge(columns)
+        .attr("rowspan", (d) => +d+1)
+        .select("span")
+        .text((d: string) => d);
+
 }
-
-// The initial display.
-update(alphabet);
-
-function onAlphabetChanged(data?: string[]) {
-    if (!data) {
-        data = d3.shuffle(alphabet)
-            .slice(0, Math.floor(Math.random() * 26))
-            .sort();
-        alphabetInput.attr("value", data.join(""));
-    }
-    update(data);
-}
-
-// Grab a random sample of letters from the alphabet, in alphabetical order.
-let interval = d3.interval(() => onAlphabetChanged(), 1500);
-
-alphabetInput.on("input", function () {
-    onAlphabetChanged((<HTMLInputElement>this).value.split(""));
-    interval.stop();
-});
