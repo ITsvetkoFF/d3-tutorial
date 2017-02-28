ff9577ccd06aa192d40f004bd9098dcd1bf538fa
itsvetkoff <tsvetkoff@i.ua>
2017-02-28 05:45:51 +0200

    and drag effect is there

---
+++ lesson3/script.js
@@ -0,0 +1,117 @@
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function (d) {
+        return d.id;
+    }).distance(250))
+    .force("collide", d3.forceCollide(50))
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+svg.append("defs")
+    .append("marker")
+    .attr("id", "arrow")
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 15)
+    .attr("refY", -1.5)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("orient", "auto")
+    .append("path")
+    .attr("d", "M0,-5L10,0L0,5");
+
+d3.json("../scripts/deps-d3.json", function (error, graph) {
+    if (error) throw error;
+
+    var link = svg.append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(graph.links)
+        .enter()
+        .append("line")
+        .attr("class", d => d.type)
+        .attr("marker-end", "url(#arrow");
+
+    var node = svg.append("g")
+        .attr("class", "nodes")
+        .selectAll("text")
+        .data(graph.nodes)
+        .enter().append("text")
+        .text(d => d.id)
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
+    node.append("title")
+        .text(function (d) {
+            return d.id;
+        });
+
+    simulation
+        .nodes(graph.nodes)
+        .on("tick", ticked);
+
+    simulation.force("link")
+        .links(graph.links);
+
+    function ticked() {
+        link
+            .attr("x1", function (d) {
+                return d.source.x;
+            })
+            .attr("y1", function (d) {
+                return d.source.y;
+            })
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            })
+            .classed("hidden", d => !d.target.visible || !d.source.visible);
+
+        node
+            .attr("x", function (d) {
+                return d.x;
+            })
+            .attr("y", function (d) {
+                return d.y;
+            })
+            .classed("hidden", d => !d.visible)
+    }
+
+    function dragstarted(d) {
+        graph.nodes.forEach(node => {
+            if (node.id !== d.id) {
+                node.visible = false;
+            }
+        });
+        graph.links.forEach(link => {
+            if (link.source.id === d.id || link.target.id === d.id) {
+                link.source.visible = true;
+                link.target.visible = true;
+            }
+        })
+
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+        graph.nodes.forEach(node => {
+            node.visible = true;
+        });
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    }
+
+});
